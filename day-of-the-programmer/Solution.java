d3c0ae93de5697e41a6df6a9cd9aad69b6aca955
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-09 15:06:35 +0200

    solution

--- day-of-the-programmer/Solution.java
+++ day-of-the-programmer/Solution.java
@@ -12,6 +12,21 @@ public class Solution {
      * @param 
      */
     static String solve(int year) {
+        if(year < 1918){
+            if( year % 4 == 0 ){
+                return "12.09."+year;
+            }else{
+                return "13.09."+year;
+            }
+        }else if(year == 1918){
+            return "26.09."+year;
+        }else{
+            if( year%400 ==0 || (year%4 == 0 && year%100 != 0)){
+                return "12.09."+year;
+            }else{
+                return "13.09."+year;
+            }
+        }
 
 
     }
