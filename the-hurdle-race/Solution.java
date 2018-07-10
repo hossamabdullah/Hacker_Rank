26c840ec6f2f4a26edae2e611b96fe3b60e43309
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-10 13:40:40 +0200

    solution

---
+++ the-hurdle-race/Solution.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /**
+     * @param k how long kan Dan jumb
+     * @param height heights of obstacles
+     */
+    static int hurdleRace(int k, int[] heights) {
+        int maxHeight = 0;
+        for(int height: heights){
+            if(height > maxHeight)
+                maxHeight = height;
+        }
+        if(maxHeight > k)
+            return maxHeight - k;
+        else
+            return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] height = new int[n];
+
+        String[] heightItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int heightItem = Integer.parseInt(heightItems[i]);
+            height[i] = heightItem;
+        }
+
+        int result = hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
