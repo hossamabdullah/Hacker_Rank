21b0e14b34401d4424ecccee3f9d9165d0d4a197
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-13 22:16:34 +0200

    solution to magic squre but a bad one

--- magic-square-forming/Solution.java
+++ magic-square-forming/Solution.java
@@ -10,7 +10,35 @@ public class Solution {
 
     // Complete the formingMagicSquare function below.
     static int formingMagicSquare(int[][] s) {
+        int[][][] sols =  {
+            {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+            {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+            {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+            {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}}, 
+            {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+            {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}}, 
+            {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}}, 
+            {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}},
+        };
 
+        int lowest_diff = 100;
+        for(int x=0; x<8; x++){
+            int[][] sol = sols[x];
+            int diff = 0;
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    int temp = s[i][j] - sol[i][j];
+                    if(temp < 0)
+                        temp *= -1;
+                    
+                    diff += temp;
+                }
+            }
+            
+            if(diff < lowest_diff)
+                lowest_diff = diff;
+        }
+        return lowest_diff;
 
     }
 
