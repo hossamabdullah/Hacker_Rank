f50d6b4490af61233710376234a84f148c7d4b43
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-14 12:24:51 +0200

    adding viral advertising and first solution

---
+++ Viral Advertising/Solution.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        int liked = 5/2;
+        int cumulative = 5/2;
+        int dayNum = 2;
+
+        while(dayNum <= n){
+            int shared = liked * 3;
+            liked = shared / 2;
+            cumulative += liked;
+            dayNum++;
+        }   
+
+        return cumulative;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
