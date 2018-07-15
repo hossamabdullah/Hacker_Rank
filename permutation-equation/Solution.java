93064ebfac81bbf2d09afa31525214351022e4ef
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-15 15:34:14 +0200

    solution

---
+++ permutation-equation/Solution.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the permutationEquation function below.
+    static int[] permutationEquation(int[] p) {
+        for(int i=0; i<p.length; i++){
+            System.out.println(p[i]);
+        }
+
+        int[] temp = new int[p.length];
+        for(int i=0; i<p.length; i++){
+            temp[p[i]-1] = i+1;
+        }
+
+        for(int i=0; i<p.length; i++){
+            System.out.println(temp[i]);
+        }
+        
+        int[] solution = new int[p.length];
+        for(int i=0; i<p.length; i++){
+            solution[i] = temp[temp[i]];
+        }
+
+        return solution;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] p = new int[n];
+
+        String[] pItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pItem = Integer.parseInt(pItems[i]);
+            p[i] = pItem;
+        }
+
+        int[] result = permutationEquation(p);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
