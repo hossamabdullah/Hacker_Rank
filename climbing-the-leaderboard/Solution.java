c6c3f8e5a0ccb3b4e5391ba1bbe973a5670b474c
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-16 16:38:38 +0200

    trying first solution to climbing the leaderboard

--- climbing-the-leaderboard/Solution.java
+++ climbing-the-leaderboard/Solution.java
@@ -6,12 +6,38 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+import javax.lang.model.util.ElementScanner6;
+
 public class Solution {
 
     // Complete the climbingLeaderboard function below.
     static int[] climbingLeaderboard(int[] scores, int[] alice) {
+        int[] rankings = new int[scores.length];
+        rankings[0] = 1;
+        for(int i=1; i<scores.length; i++){
+            if(scores[i] == scores[i-1])
+                rankings[i] = rankings[i-1];
+            else
+                rankings[i] = rankings[i-1] + 1;
+        }
+
+        for(int i=0; i<scores.length; i++){
+            System.out.println(rankings[i]);
+        }
+        System.out.println("done validating rankings");
 
 
+        int[] solutions = new int[alice.length];
+        for(int j=0; j<alice.length; j++){
+            for(int i=0; i<scores.length; i++){
+                if(alice[j] == scores[i])
+                    solutions[j] = rankings[i];
+                else if(alice[j] > scores[i])
+                    solutions[j] = rankings[i] - 1;
+
+            }
+        }
+        return solutions;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
