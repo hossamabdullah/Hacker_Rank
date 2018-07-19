4774b30f47255a54d447e365a3107815d4e5b819
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-19 14:50:35 +0200

    complete solution

--- library-fine/Solution.java
+++ library-fine/Solution.java
@@ -10,9 +10,13 @@ public class Solution {
 
     // Complete the libraryFine function below.
     static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        if(y1 < y2)
+            return 0;
         if(y1 > y2)
             return 10000;
 
+        if(m1 < m2)
+            return 0;
         if(m1 > m2)
             return 500*(m1-m2);
         
