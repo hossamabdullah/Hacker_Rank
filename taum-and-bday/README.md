5e051e563a7b0d5085ac8db469655c7df5826be2
HossamAbdullah <hossamabdalh@gmail.com>
2018-07-23 09:44:12 +0200

    initial repo for Taum and B'day

---
+++ taum-and-bday/README.md
@@ -0,0 +1,2 @@
+This is the link for the problem
+https://www.hackerrank.com/challenges/taum-and-bday/problem
---
+++ taum-and-bday/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the taumBday function below.
+    static int taumBday(int b, int w, int bc, int wc, int z) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] bw = scanner.nextLine().split(" ");
+
+            int b = Integer.parseInt(bw[0]);
+
+            int w = Integer.parseInt(bw[1]);
+
+            String[] bcWcz = scanner.nextLine().split(" ");
+
+            int bc = Integer.parseInt(bcWcz[0]);
+
+            int wc = Integer.parseInt(bcWcz[1]);
+
+            int z = Integer.parseInt(bcWcz[2]);
+
+            int result = taumBday(b, w, bc, wc, z);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
