33665a6b63c6dfe338a08aaf2890622ef900a61a
HossamAbdullah <hossamabdalh@gmail.com>
2018-08-13 10:58:41 +0200

    trying different group_cocat

--- SQL/occupations/Solution.sql
+++ SQL/occupations/Solution.sql
@@ -1,6 +1,11 @@
-SELECT  
+SELECT
   GROUP_CONCAT(if(occupation = 'Doctor', name, NULL)) AS 'Doctor', 
   GROUP_CONCAT(if(occupation = 'Professor', name, NULL)) AS 'Professor', 
   GROUP_CONCAT(if(occupation = 'Singer', name, NULL)) AS 'Singer',
   GROUP_CONCAT(if(occupation = 'Actor', name, NULL)) AS 'Actor' 
-FROM occupations
+FROM occupations 
+
+SELECT GROUP_CONCAT(name) as groupname,  occupation
+from occupations
+group by occupation
+order by groupname
