bb7883b76f9129213988792a5a669c0937e02c2a
HossamAbdullah <hossamabdalh@gmail.com>
2018-08-18 11:37:36 +0200

    working on python

---
+++ Python/new_prop.py
@@ -0,0 +1,26 @@
+import sys
+
+link = sys.argv[1]
+temp = link
+start_index = 38
+link = link[start_index:]
+end_index = link.find('/')
+link = link[:end_index]
+
+print(link)
+import os
+if not os.path.exists(link):
+    os.makedirs(link)
+
+file = open(link+'/README.md','w') 
+file.write('this is the link for the problem \n')
+file.write(temp)
+file.close()
+
+file = open(link+'/Solution.py','w') 
+file.write('')
+file.close()
+
+from subprocess import call
+call(["git", "add", "."])
+call(["git", "commit", "-m", "initial directory for "+link])
