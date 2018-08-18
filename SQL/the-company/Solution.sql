2bde9127e39d112bb4565d9be8ba280018297797
HossamAbdullah <hossamabdalh@gmail.com>
2018-08-18 11:17:04 +0200

    trying distinct

--- SQL/the-company/Solution.sql
+++ SQL/the-company/Solution.sql
@@ -1,10 +1,10 @@
 select 
 c.company_code, 
 c.founder, 
-count(l.lead_manager_code), 
-count(s.senior_manager_code),
-count(m.manager_code),
-count(e.employee_code)
+count(distinct l.lead_manager_code), 
+count(distinct s.senior_manager_code),
+count(distinct m.manager_code),
+count(distinct e.employee_code)
 from 
 company c, lead_manager l, senior_manager s, manager m, employee e
 where
@@ -12,4 +12,5 @@ c.company_code = l.company_code and
 l.lead_manager_code = s.lead_manager_code and
 s.senior_manager_code = m.senior_manager_code and
 m.manager_code = e.manager_code
+group by c.company_code, c.founder
 order by c.company_code
