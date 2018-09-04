398e550fc48dcdbe866420188cd52efa6b462a3c
HossamAbdullah <hossamabdalh@gmail.com>
2018-09-04 14:54:13 +0200

    create list of all scores including alice

--- Algorithms/climbing-the-leaderboard/Solution.java
+++ Algorithms/climbing-the-leaderboard/Solution.java
@@ -8,21 +8,91 @@ import java.util.regex.*;
 
 import javax.lang.model.util.ElementScanner6;
 
+class Rank{
+    public int score;
+    public boolean isAlice;
+    public int rank;
+
+    public Rank(int score, boolean isAlice){
+        this.score = score;
+        this.isAlice = isAlice;
+    }
+
+    public Rank(int score, boolean isAlice, int rank){
+        this.score = score;
+        this.isAlice = isAlice;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return score+", "+isAlice+", "+rank;
+    }
+}
+
 public class Solution {
 
     // Complete the climbingLeaderboard function below.
     static int[] climbingLeaderboard(int[] scores, int[] alice) {
-        int[] rankings = new int[scores.length];
-        rankings[0] = 1;
-        for(int i=1; i<scores.length; i++){
-            if(scores[i] == scores[i-1]){
-                rankings[i] = rankings[i-1];
+        int fullLength = scores.length + alice.length;
+        Rank[] rankings = new Rank[fullLength];
+        
+        int minLength = 0;
+        if(scores.length < alice.length)
+            minLength = scores.length;
+        else   
+            minLength = alice.length;
+        
+        //Merge concatenation
+        for(int i=0; i<minLength; i++){
+            Rank temp = null;
+            if(scores[i] > alice[i]){
+                temp = new Rank(scores[i], false);
             }else{
-                rankings[i] = rankings[i-1] + 1;
+                temp = new Rank(alice[i], true);
             }
-            System.out.println(rankings[i]);
+            
+            if(i==0)
+                temp.rank = 1;
+            else if(temp.score == rankings[i-1].score)
+                temp.rank = rankings[i-1].rank;
+            else if(!rankings[i-1].isAlice)
+                temp.rank = rankings[i-1].rank + 1;
+            else
+                temp.rank = rankings[i-1].rank;
+
+            rankings[i] = temp;
+        }
+
+        int index = minLength;
+        while(minLength < scores.length){
+            Rank temp = new Rank(scores[index], false);
+            if(temp.score == rankings[index-1].score)
+                temp.rank = rankings[index-1].rank;
+            else if(!rankings[index-1].isAlice)
+                temp.rank = rankings[index-1].rank + 1;
+            else
+                temp.rank = rankings[index-1].rank;
+            rankings[index] = temp;
+            index ++;
+        }
+
+        index = minLength;
+        while(minLength < alice.length){
+            Rank temp = new Rank(scores[index], true);
+            if(temp.score == rankings[index-1].score)
+                temp.rank = rankings[index-1].rank;
+            else if(!rankings[index-1].isAlice)
+                temp.rank = rankings[index-1].rank + 1;
+            else
+                temp.rank = rankings[index-1].rank;
+            rankings[index] = temp;
+            index ++;
         }
         
+        for(int i=0; i<rankings.length; i++){
+            System.out.println(rankings[i]);
+        }
         return null;
     }
 
@@ -59,18 +129,18 @@ public class Solution {
 
         int[] result = climbingLeaderboard(scores, alice);
 
-        for (int i = 0; i < result.length; i++) {
-            bufferedWriter.write(String.valueOf(result[i]));
+        // for (int i = 0; i < result.length; i++) {
+        //     bufferedWriter.write(String.valueOf(result[i]));
 
-            if (i != result.length - 1) {
-                bufferedWriter.write("\n");
-            }
-        }
+        //     if (i != result.length - 1) {
+        //         bufferedWriter.write("\n");
+        //     }
+        // }
 
-        bufferedWriter.newLine();
+        // bufferedWriter.newLine();
 
-        bufferedWriter.close();
+        // bufferedWriter.close();
 
-        scanner.close();
+        // scanner.close();
     }
 }
