7176770781dada17a453f8c2ea6d098398ab6029
HossamAbdullah <hossamabdalh@gmail.com>
2018-09-04 15:35:42 +0200

    remove comment

--- Algorithms/climbing-the-leaderboard/Solution.java
+++ Algorithms/climbing-the-leaderboard/Solution.java
@@ -97,7 +97,7 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int scoresCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
