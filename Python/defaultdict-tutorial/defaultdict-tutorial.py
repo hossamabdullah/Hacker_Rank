1c8b4bc7a9b844f047127d9cc9aaeded958b7d91
HossamAbdullah <hossamabdalh@gmail.com>
2018-12-05 13:39:29 +0200

    better formatted output

--- Python/defaultdict-tutorial/defaultdict-tutorial.py
+++ Python/defaultdict-tutorial/defaultdict-tutorial.py
@@ -1,20 +1,12 @@
 from collections import defaultdict
-def format_output(data_array):
-    formatted_output = ""
-    for element in data_array:
-        formatted_output += str(element + 1) + " "
-    return formatted_output
-
 d = defaultdict(list)
 
-n, m = input().split()
-n = int(n)
-m = int(m)
+n, m = map(int, input().split())
 
 for i in range(n):
     n_a = input()
-    d[n_a].append(i)
+    d[n_a].append(i+1)
 
 for i in range(m):
     n_b = input()
-    print(format_output(d[n_b]))
+    print(*d[n_b], sep=' ')
