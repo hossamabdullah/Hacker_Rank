57f64a829fb81122e389c7ceb245d00a8492a952
HossamAbdullah <hossamabdalh@gmail.com>
2019-03-18 13:49:47 +0200

    first solution to big sort

---
+++ Algorithms/big-sorting/Solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    // Complete the bigSorting function below.
+    static String[] bigSorting(String[] unsorted) {
+        List<BigInteger> list = new ArrayList<BigInteger>();
+        for(String elem : unsorted){
+            list.add(new BigInteger(elem));
+        }
+        Collections.sort(list);
+        for(int i=0; i<unsorted.length; i++){
+            unsorted[i] = list.get(i).toString();
+        }
+        return unsorted;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] unsorted = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String unsortedItem = scanner.nextLine();
+            unsorted[i] = unsortedItem;
+        }
+
+        String[] result = bigSorting(unsorted);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(result[i]);
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
