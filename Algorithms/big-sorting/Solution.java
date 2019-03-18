fdf99da14ece8600fa23f0f412bc01bdc9a15871
HossamAbdullah <hossamabdalh@gmail.com>
2019-03-18 14:08:21 +0200

    second solution to big sort

--- Algorithms/big-sorting/Solution.java
+++ Algorithms/big-sorting/Solution.java
@@ -10,14 +10,34 @@ public class Solution {
     
     // Complete the bigSorting function below.
     static String[] bigSorting(String[] unsorted) {
-        List<BigInteger> list = new ArrayList<BigInteger>();
-        for(String elem : unsorted){
-            list.add(new BigInteger(elem));
-        }
-        Collections.sort(list);
-        for(int i=0; i<unsorted.length; i++){
-            unsorted[i] = list.get(i).toString();
-        }
+        // List<BigInteger> list = new ArrayList<BigInteger>();
+        // for(String elem : unsorted){
+        //     list.add(new BigInteger(elem));
+        // }
+        // Collections.sort(list);
+        // for(int i=0; i<unsorted.length; i++){
+        //     unsorted[i] = list.get(i).toString();
+        // }
+        // return unsorted;
+        Arrays.sort(unsorted, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() < o2.length()){
+                    return -1;
+                }else if(o1.length() > o2.length()){
+                    return 1;
+                }else{
+                    BigInteger a = new BigInteger(o1);
+                    BigInteger b = new BigInteger(o2);
+                    if(a.compareTo(b) > 0){
+                        return 1;
+                    }else{
+                        return -1;
+                    }
+                }
+                
+            }
+        });
         return unsorted;
     }
 
