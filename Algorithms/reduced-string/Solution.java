31c7cc3e5e3bcce39ceca59d774797bd63867418
HossamAbdullah <hossamabdalh@gmail.com>
2019-05-13 12:59:38 +0200

    first try for reduced string

---
+++ Algorithms/reduced-string/Solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the superReducedString function below.
+    static String superReducedString(String s) {
+        int i = 0;
+        while(true){
+            if(s.charAt(i) == s.charAt(i+1)){
+                s = s.substring(0, i) + s.substring(i+1, s.length());
+            }else{
+                i++;
+            }
+
+            if(s.length() == 0 || i >= s.length())
+                break;
+            
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = superReducedString(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
