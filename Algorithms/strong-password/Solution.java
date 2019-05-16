c8d5c7416af41135d8617708ae3b97b025988c68
HossamAbdullah <hossamabdalh@gmail.com>
2019-05-16 02:41:39 +0200

    solution for strong password

---
+++ Algorithms/strong-password/Solution.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumNumber function below.
+    static int minimumNumber(int n, String password) {
+        // Return the minimum number of characters to make the password strong
+        String numbers = "0123456789";
+        String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String specialCharacters = "!@#$%^&*()-+";
+
+        boolean numbersFlag = false, lowerCaseFlag = false, upperCaseFlag = false, specialCharactersFlag= false;
+
+        for(int i=0; i<password.length(); i++){
+            String currentChar = password.charAt(i)+"";
+            if(numbers.contains(currentChar))
+                numbersFlag =true;
+            else if(lowerCase.contains(currentChar))
+                lowerCaseFlag =true;
+            else if(upperCase.contains(currentChar))
+                upperCaseFlag=true;
+            else if(specialCharacters.contains(currentChar))
+                specialCharactersFlag = true;
+        }
+
+        int count=  0;
+        if(!numbersFlag)
+            count ++;
+        if(!lowerCaseFlag)
+            count ++;
+        if(!upperCaseFlag)
+            count ++;
+        if(!specialCharactersFlag)
+            count ++;
+        
+        if(n+count < 6){
+            int temp = n + count;
+            count += 6 - (temp);
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String password = scanner.nextLine();
+
+        int answer = minimumNumber(n, password);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
