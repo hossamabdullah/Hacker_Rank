a22eff0922ea629c4ecefa080208c89977ea9ac6
HossamAbdullah <hossamabdalh@gmail.com>
2019-05-18 15:03:33 +0200

    prepare a reflection answer - dynamic programming

--- Algorithms/bigger-is-greater/Solution.java
+++ Algorithms/bigger-is-greater/Solution.java
@@ -8,63 +8,80 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static char[] swap(char[] ar, int firstIndex, int secondIndex){
-        char temp = ar[firstIndex];
-        ar[firstIndex] = ar[secondIndex];
-        ar[secondIndex] = temp;
-        return ar;
-    }
+    static String bigIsGr(String w){
+        String prev = "";
+        String restOfWord = w.substring(1);
+        if(w.length() != 1){
+            prev = bigIsGr(restOfWord);
+        }
 
-    static char[] sortFromTo(char[] ar, int st, int en){
-        for(int i=st; i<en-1; i++){
-            int min = 1000;
-            int ind = 1000;
-            for(int j=st+1; j<en; j++){
-                if(ar[j] < min){
-                    min = ar[j];
-                    ind = j;
+        
+        char s = w.charAt(0);
+        if(w.length() == 1){
+            //only one character in w, lets just return it
+            return new String(s);
+        }else{
+            //char s     String prev
+            if(prev.compareTo(restOfWord) > 0){
+                return s + prev;
+            }else{
+                char[] prevC = prev.toCharArray();
+                for(int i=0; i<prevC.length; i++){
+                    
                 }
             }
-            ar = swap(ar, ind, i);
         }
-        return ar;
+        
+        return null;
     }
 
+    
     // Complete the biggerIsGreater function below.
     static String biggerIsGreater(String w) {
         char[] c = w.toCharArray();
-        int index = 1000;
-        int min = 1000;
-        for(int i=c.length-1; i>=0; i--){
-            for(int j=i+1; j<c.length; j++){
-                if(c[j] > c[i] && c[j] < min){
-                    min = c[j];
-                    index = j;
-                }    
+        boolean descSorted = true;
+        for(int i=0; i<c.length-1; i++){
+            if(c[i] < c[i+1])
+                descSorted = false;
+        }
+        if(descSorted)
+            return "no answer";
+
+        List<Character> n = new ArrayList<Character>();
+        n.add(c[c.length-1]);
+
+        int foundIndex = 1000;
+        for(int i=c.length-2; i>=0; i--){
+            boolean biggerThanAll = true;
+            if(foundIndex != 1000){
+                System.out.println(c[i]);
+                // n.add(0, c[i]);
             }
-            if(index != 1000){
-                swap(c, index, i);
-                sortFromTo(c, i+1, c.length);
+            for(int j=0; j<n.size(); j++){
+                if(c[i] < n.get(j)){
+                    biggerThanAll = false;
+                    foundIndex = j;
+                }
+            }
+            if(biggerThanAll){
+                n.add(c[i]);
+            }else{
+                char temp = n.get(foundIndex);
+                n.remove(foundIndex);
+                n.add(foundIndex, c[i]);
+                n.add(0, temp);
                 break;
             }
         }
-        if(index == 1000){
-            return "no answer";
-        }
-        return new String(c);
+
+        System.out.println(n.size());
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : n) { 
+            sb.append(ch); 
+        } 
+        String result = sb.toString();
+        return result;
     }
-    // // Complete the biggerIsGreater function below.
-    // static String biggerIsGreater(String w) {
-    //     char[] c = w.toCharArray();
-    //     for(int i=c.length-1; i>=0; i--){
-    //         System.out.print(c[i]);
-    //         for(int j=i+1; j<c.length; j++){
-    //             System.out.print(c[j]);
-    //         }
-    //         System.out.println();
-    //     }
-    //     return "";
-    // }
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -77,9 +94,9 @@ public class Solution {
         for (int TItr = 0; TItr < T; TItr++) {
             String w = scanner.nextLine();
 
-            String result = biggerIsGreater(w);
+            String result = bigIsGr(w);
 
-            bufferedWriter.write(result);
+            // bufferedWriter.write(result);
             bufferedWriter.newLine();
         }
 
