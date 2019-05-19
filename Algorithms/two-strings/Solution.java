6696b5c4a5749a031ef56d253a60066d29782a1d
HossamAbdullah <hossamabdalh@gmail.com>
2019-05-19 14:05:39 +0200

    first solution to two-strings

---
+++ Algorithms/two-strings/Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        String yes = "YES";
+        String no = "NO";
+        String result = no;
+        char[] s1Chars = s1.toCharArray();
+        char[] s2Chars = s2.toCharArray();
+
+        for(int i=0; i<s1Chars.length; i++){
+            for(int j=0; j<s2Chars.length; j++){
+                if(s1Chars[i] == s2Chars[j]){
+                    result = yes;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
