d0b9e2a82611fa732bf54dc2a70e73c7da56b113
HossamAbdullah <hossamabdalh@gmail.com>
2019-08-06 11:36:26 +0200

    solution for seprate the numbers

---
+++ Algorithms/separate-the-numbers/Solution.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the separateNumbers function below.
+    static void separateNumbers(String s) {
+        // System.out.println(s);
+        int length = s.length();
+        int halfLength = length/2;
+
+        int range = 1;
+        Integer minStart = null;
+        while(range <= halfLength){
+            boolean seqFound = true;
+            boolean rangeIncreased = false;
+
+            for(int i=range; (i+range)<=length; i+=range){
+                // System.out.println(i +", "+ range);
+                int prev = Integer.parseInt(s.substring(i-range, i));
+                
+                if((prev+1) % 10 == 0){
+                    range++;
+                    rangeIncreased = true;
+                }
+                int current = Integer.parseInt(s.substring(i, i+range));
+                // System.out.println(current+" - "+prev);
+                if(current - prev != 1){
+                    seqFound = false;
+                    break;
+                }
+            }
+            if(seqFound){
+                if(rangeIncreased)
+                    minStart = Integer.parseInt(s.substring(0, range-1));
+                else
+                    minStart = Integer.parseInt(s.substring(0, range));
+                break;
+            }else{
+                if(!rangeIncreased)
+                    range++;
+            }
+        }
+
+
+        if(minStart == null){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES "+minStart);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            separateNumbers(s);
+        }
+
+        scanner.close();
+    }
+}
