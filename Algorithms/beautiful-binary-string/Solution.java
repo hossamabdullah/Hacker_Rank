f8f3a90079a3759ce55a6bc5d1db83aa47d2e402
HossamAbdullah <hossamabdalh@gmail.com>
2020-01-07 11:46:10 +0200

    first solution to beautiful binary string

---
+++ Algorithms/beautiful-binary-string/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the beautifulBinaryString function below.
+    static int beautifulBinaryString(String b) {
+        String keyword = "010";
+        int index = 0;
+        int count = 0;
+        while(index+3 <= b.length()){
+            if(b.substring(index, index+3).equals(keyword)){
+                index +=3;
+                count ++;
+            }else{
+                index +=1;
+            }
+        }
+        return count;
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String b = scanner.nextLine();
+
+        int result = beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
