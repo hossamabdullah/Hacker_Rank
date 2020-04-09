0a66a4ef7919d3297500deeaebdb7246c2c9d6a7
HossamAbdullah <hossamabdalh@gmail.com>
2020-04-09 11:28:52 +0200

    solution to lowest_triange

---
+++ Mathematics/lowest-triangle/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int lowestTriangle(int base, int area){
+        //area rule = 1/2 * base * height
+        //heigt = 2* area/ base 
+        float height = (float)(2*area) / base;
+        return (int) Math.ceil(height);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int base = in.nextInt();
+        int area = in.nextInt();
+        int height = lowestTriangle(base, area);
+        System.out.println(height);
+    }
+}
