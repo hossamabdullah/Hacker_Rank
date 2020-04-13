b41e22a94f65037782d6572d3175295bfab04402
HossamAbdullah <hossamabdalh@gmail.com>
2020-04-13 11:20:58 +0200

    solution to p1-paper-cutting

---
+++ Mathematics/p1-paper-cutting/Solution.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static long solve(int n, int m) {
+
+        long stepOne = n-1;
+        long stepTwo = stepOne * m;
+        long stepThree = stepTwo + (m-1);
+        return stepThree;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        long result = solve(n, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
