1c32cabe33a16f7a1fa6c6ec15b28cd6f7b8e1e0
HossamAbdullah <hossamabdalh@gmail.com>
2020-04-19 01:34:04 +0200

    first solution to game-of-thrones

---
+++ Algorithms/game-of-thrones/Solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the gameOfThrones function below.
+    static String gameOfThrones(String s) {
+        Set<Character> found = new HashSet<Character>();
+        for(char temp: s.toCharArray()){
+            if(found.contains(temp))
+                found.remove(temp);
+            else
+                found.add(temp);
+        }
+        if(found.size() <= 1)
+            return "YES";
+        else
+            return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = gameOfThrones(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
