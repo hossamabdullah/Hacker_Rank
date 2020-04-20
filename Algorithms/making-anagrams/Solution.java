f820339e10e7b2a249f6f11947061d2a84a23821
HossamAbdullah <hossamabdalh@gmail.com>
2020-04-20 17:50:58 +0200

    solution to maing anagrams

--- Algorithms/making-anagrams/Solution.java
+++ Algorithms/making-anagrams/Solution.java
@@ -10,34 +10,25 @@ public class Solution {
 
     // Complete the makingAnagrams function below.
     static int makingAnagrams(String s1, String s2) {
-        System.out.println(s1);
-        System.out.println(s2);
-        int maxLength = 0;
         char[] s1Chars = s1.toCharArray();
         char[] s2Chars = s2.toCharArray();
 
-        String choosed = "";
-        for(int a= 0 ; a < s1Chars.length; a++){
-            for(int b = 0; b < s2Chars.length; b++){
-                int length = 0;
-                if(s1Chars[a] == s2Chars[b]){
-                    length++;
-                    String word=  s1Chars[a] + "";
-                    for(int aa=a+1, bb=b+1; (aa<s1Chars.length && bb<s2Chars.length && s1Chars[aa] == s2Chars[bb]); aa++, bb++){
-                        length++;
-                        word = word + s1Chars[aa];
-                    }
-                    if(length > maxLength){
-                        maxLength = length;
-                        choosed = word;
-                    }
-                }
-            }
+        int[] char1Occs = new int[26];
+        int[] char2Occs = new int[26];
+        for(int i= 0; i< s1Chars.length; i++){
+            char1Occs[s1Chars[i] - 'a']++;
         }
 
-        System.out.println(maxLength);
-        System.out.println(choosed);
-        return (s1Chars.length - maxLength + s2Chars.length - maxLength);
+        for(int i= 0; i< s2Chars.length; i++){
+            char2Occs[s2Chars[i] - 'a']++;
+        }
+
+        int difference = 0;
+        for(int i=0; i<26; i++){
+            difference += Math.abs(char1Occs[i] - char2Occs[i]);
+        }
+
+        return difference;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
