667f457bf028cf09c8cdc69f127f43bdb04d5aee
HossamAbdullah <hossamabdalh@gmail.com>
2020-04-20 17:38:37 +0200

    end of path of first solution

--- Algorithms/making-anagrams/Solution.java
+++ Algorithms/making-anagrams/Solution.java
@@ -10,27 +10,33 @@ public class Solution {
 
     // Complete the makingAnagrams function below.
     static int makingAnagrams(String s1, String s2) {
+        System.out.println(s1);
+        System.out.println(s2);
         int maxLength = 0;
         char[] s1Chars = s1.toCharArray();
         char[] s2Chars = s2.toCharArray();
 
+        String choosed = "";
         for(int a= 0 ; a < s1Chars.length; a++){
-            int length = 0;
             for(int b = 0; b < s2Chars.length; b++){
+                int length = 0;
                 if(s1Chars[a] == s2Chars[b]){
                     length++;
-                    for(int aa=a+1, bb=b+1; aa<s1Chars.length && bb<s2Chars.length; aa++, bb++){
-                        if(s1Chars[aa] == s2Chars[bb]){
-                            length++;
-                        }
+                    String word=  s1Chars[a] + "";
+                    for(int aa=a+1, bb=b+1; (aa<s1Chars.length && bb<s2Chars.length && s1Chars[aa] == s2Chars[bb]); aa++, bb++){
+                        length++;
+                        word = word + s1Chars[aa];
                     }
-                    if(length > maxLength)
+                    if(length > maxLength){
                         maxLength = length;
+                        choosed = word;
+                    }
                 }
             }
         }
 
         System.out.println(maxLength);
+        System.out.println(choosed);
         return (s1Chars.length - maxLength + s2Chars.length - maxLength);
     }
 
