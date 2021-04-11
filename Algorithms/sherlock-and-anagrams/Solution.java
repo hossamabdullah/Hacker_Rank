a48f9b3c8a7bb402cdb080acbf998d7d7a8974bc
HossamAbdullah <hossamabdalh@gmail.com>
2021-04-11 05:58:53 +0200

    initial solution

--- Algorithms/sherlock-and-anagrams/Solution.java
+++ Algorithms/sherlock-and-anagrams/Solution.java
@@ -1,49 +1,68 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+    import java.io.*;
+    import java.math.*;
+    import java.security.*;
+    import java.text.*;
+    import java.util.*;
+    import java.util.concurrent.*;
+    import java.util.regex.*;
+    import java.util.stream.Collectors;
 
-public class Solution {
+    public class Solution {
 
-    // Complete the sherlockAndAnagrams function below.
-    static int sherlockAndAnagrams(String s) {
-        char[] data = s.toCharArray();
-        Set<Character> dataDuplicateFree = convertFromArrayToSet(data);
-        if(dataDuplicateFree.size() == data.length)
-            return 0;
-        else
-            return 2;
-    }
+        // Complete the sherlockAndAnagrams function below.
+        static int sherlockAndAnagrams(String s) {
+            int count = 0;
+            for(int i=1; i<s.length(); i++){
+                Map<String, Integer> results = new HashMap<>();
+                for(int j=0; j<s.length() && j+i <= s.length(); j++){
+                    String slice = s.substring(j, j+i);
+                    slice = sortString(slice);
+                    System.out.println(slice);
 
-    private static Set<Character> convertFromArrayToSet(char[] data){
-        Set<Character> dataDuplicateFree = new HashSet<>();
-        for(int i=0; i<data.length; i++){
-            dataDuplicateFree.add(data[i]);
-        }
-        return dataDuplicateFree;
-    }
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int qItr = 0; qItr < q; qItr++) {
-            String s = scanner.nextLine();
-
-            int result = sherlockAndAnagrams(s);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
+                    if(results.containsKey(slice)){
+                        count += results.get(slice);
+                        int newCount = count + 1;
+                        results.put(slice, newCount);
+                    }else{
+                        results.put(slice, 1);
+                    }
+                }
+            }
+            return count;
         }
 
-        bufferedWriter.close();
+        public static String sortString(String inputString){
+            char tempArray[] = inputString.toCharArray();
+            Arrays.sort(tempArray);
+            return new String(tempArray);
+        }
 
-        scanner.close();
+        private static Set<Character> convertFromArrayToSet(char[] data){
+            Set<Character> dataDuplicateFree = new HashSet<>();
+            for(int i=0; i<data.length; i++){
+                dataDuplicateFree.add(data[i]);
+            }
+            return dataDuplicateFree;
+        }
+        private static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) throws IOException {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            int q = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int qItr = 0; qItr < q; qItr++) {
+                String s = scanner.nextLine();
+
+                int result = sherlockAndAnagrams(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.close();
+
+            scanner.close();
+        }
     }
-}
