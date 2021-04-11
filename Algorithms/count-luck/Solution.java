50b1fb9e79c50102c8e4247077de6f77198d54e1
HossamAbdullah <hossamabdalh@gmail.com>
2021-04-11 18:16:10 +0200

    intial solution to count lucj :

--- Algorithms/count-luck/Solution.java
+++ Algorithms/count-luck/Solution.java
@@ -9,13 +9,14 @@ import java.util.regex.*;
 public class Solution {
 
     // Complete the countLuck function below.
-    static int countLuck(char[][] matrix, int startRow, int startCol, int k) {
+    static boolean countLuck(char[][] matrix, int startRow, int startCol, int k) {
         matrix[startRow][startCol] = '.';
         int res =  countLuck(matrix, startRow, startCol, true);
-        return res;
+        if(res-1 == k) return true;
+        else           return false;
     }
 
-    static boolean isValidNode(char[][] matrix, int row, int col){
+    static int isValidNode(char[][] matrix, int currentRow, int currentCol){
         if(
             currentCol == -1
             || currentRow == -1
@@ -24,26 +25,38 @@ public class Solution {
             || matrix[currentRow][currentCol] == 'X'
             || matrix[currentRow][currentCol] == 'M'
             || matrix[currentRow][currentCol] == 'P')
-            return true;
+            return 0;
         else 
-            return false;
+            return 1;
     }
 
     static int countLuck(char[][] matrix, int currentRow, int currentCol, boolean firstTime){
-        if(isValidNode(matrix,currentRow, currentCol)) return 0;
+        if(isValidNode(matrix,currentRow, currentCol)==0) return 0;
         if(matrix[currentRow][currentCol] == '*')  return 1;
         matrix[currentRow][currentCol] = 'P'; //Passed
         
-        System.out.println(matrix[currentRow][currentCol]);
-        
-        int a = countLuck(matrix, currentRow, currentCol+1, false);
-        int b = countLuck(matrix, currentRow, currentCol-1, false);
-        int c = countLuck(matrix, currentRow+1, currentCol, false);
-        int d = countLuck(matrix, currentRow-1, currentCol, false);
+        //System.out.println(currentRow+", "+currentCol);
 
-        // if(a +b + c+ d ==0) return -1;
-        if(a + b + c + d== 1)  return 0;
-        else return 1;
+        int u = isValidNode(matrix, currentRow , currentCol+1);
+        int d = isValidNode(matrix, currentRow , currentCol-1);
+        int n = isValidNode(matrix, currentRow+1 , currentCol);
+        int p = isValidNode(matrix, currentRow-1 , currentCol);
+
+        int A=0, B=0, C=0, D = 0;
+        if(u == 1) A = countLuck(matrix, currentRow, currentCol+1, false);
+        if(d == 1) B = countLuck(matrix, currentRow, currentCol-1, false);
+        if(n == 1) C = countLuck(matrix, currentRow+1, currentCol, false);
+        if(p == 1) D = countLuck(matrix, currentRow-1, currentCol, false);
+
+        int inc = 1;
+        if(u+d+n+p == 1) inc=0;
+
+        if(A > 0) return A+inc;
+        if(B > 0) return B+inc;
+        if(C > 0) return C+inc;
+        if(D > 0) return D+inc;
+
+        return 0;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -81,9 +94,11 @@ public class Solution {
 
             
             
-            int result = countLuck(data, startRow, startCol, k);
-
-            bufferedWriter.write(result+"");
+            boolean result = countLuck(data, startRow, startCol, k);
+            String res = "";
+            if(result)  res= "Impressed";
+            else        res= "Oops!";
+            bufferedWriter.write(res+"");
             bufferedWriter.newLine();
         }
 
